b0900d1b185472dffdff491048e0434394ccf6b6
JinHyun Park <17329096+nativehyun@users.noreply.github.com>
2020-04-27 14:16:33 +0900

    Create analytics.js

---
+++ static/web/analytics.js
@@ -0,0 +1,16 @@
+<!-- Default Statcounter code for nativehyun.xyz
+https://nativehyun.xyz -->
+<script type="text/javascript">
+var sc_project=12246326; 
+var sc_invisible=1; 
+var sc_security="d2d43b2b"; 
+</script>
+<script type="text/javascript"
+src="https://www.statcounter.com/counter/counter.js"
+async></script>
+<noscript><div class="statcounter"><a title="Web Analytics"
+href="https://statcounter.com/" target="_blank"><img
+class="statcounter"
+src="https://c.statcounter.com/12246326/0/d2d43b2b/1/"
+alt="Web Analytics"></a></div></noscript>
+<!-- End of Statcounter Code -->
